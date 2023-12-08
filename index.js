1854161167896bba6d0fffa7e2f602e78597c867
Ajay-Goswami <goswamiajay2002@gmail.com>
2023-12-08 21:34:54 +0530

    responsive

--- index.js
+++ index.js
@@ -21,12 +21,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 //faq
 // document.addEventListener("DOMContentLoaded", function () {
 //   const faqItems = document.querySelectorAll(".faq-item");
