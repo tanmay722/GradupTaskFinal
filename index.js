86dc5d40d6452ff20d9fc8cf18a3ec010044a4df
Ajay-Goswami <goswamiajay2002@gmail.com>
2023-12-09 21:08:18 +0530

    updated

--- index.js
+++ index.js
@@ -34,38 +34,8 @@ function scrollFunction() {
 //   });
 // });
 
-//login-Signup
-const formOpenBtn = document.querySelector("#form-open"),
-  login_box = document.querySelector(".bodybackground"),
-  formContainer = document.querySelector(".form_container"),
-  formCloseBtn = document.querySelector(".form_close"),
-  signupBtn = document.querySelector("#signup"),
-  loginBtn = document.querySelector("#login"),
-  pwShowHide = document.querySelectorAll(".pw_hide");
-
-formOpenBtn.addEventListener("click", () => login_box.classList.add("show"));
-formCloseBtn.addEventListener("click", () =>
-  login_box.classList.remove("show")
-);
-
-pwShowHide.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let getPwInput = icon.parentElement.querySelector("input");
-    if (getPwInput.type === "password") {
-      getPwInput.type = "text";
-      icon.classList.replace("uil-eye-slash", "uil-eye");
-    } else {
-      getPwInput.type = "password";
-      icon.classList.replace("uil-eye", "uil-eye-slash");
-    }
-  });
-});
-
-signupBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.add("active");
-});
-loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.remove("active");
+// Mailbox
+document.getElementById("mailButton").addEventListener("click", function () {
+  window.location.href =
+    "mailto:teksila.ops@gmail.com?subject=Your%20Subject&body=Your%20Message%20Here";
 });
